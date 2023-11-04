9ec63ccbf87cdbb00b89efd2a42404c430ae1b51
axelrizo <axel.rizo@seistreinta.com>
2023-11-04 16:23:26 -0500

    build(auth): add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
-import { Module } from '@nestjs/common';
-import type { RedisClientOptions } from 'redis';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FinancialCalculationsModule } from './modules/financial-calculations/financial-calculations.module';
-import { ChuckNorrisJokesProxyModule } from './modules/chuck-norris-jokes-proxy/chuck-norris-jokes-proxy.module';
 import { CacheModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import type { RedisClientOptions } from 'redis';
+import { ApiKeysModule } from './modules/api-keys/api-keys.module';
+import { ExpressApiKeysTokenLimitRequests } from './modules/api-keys/middleware/api-keys.express.token-limit-requests';
+import { AuthModule } from './modules/auth/auth.module';
+import { ChuckNorrisJokesProxyModule } from './modules/chuck-norris-jokes-proxy/chuck-norris-jokes-proxy.module';
+import { DatabaseModule } from './modules/database/database.module';
+import { FinancialCalculationsModule } from './modules/financial-calculations/financial-calculations.module';
+import { UsersModule } from './modules/users/users.module';
+import { ModulesService } from './api-keys/modules/modules.service';
 
 @Module({
   imports: [
+    DatabaseModule,
     ConfigModule.forRoot(),
     CacheModule.register<RedisClientOptions>({
       store: redisStore,
@@ -19,8 +24,17 @@ import { ConfigModule } from '@nestjs/config';
     }),
     FinancialCalculationsModule,
     ChuckNorrisJokesProxyModule,
+    UsersModule,
+    AuthModule,
+    ApiKeysModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ModulesService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ExpressApiKeysTokenLimitRequests)
+      .forRoutes('chuck-norris-jokes/random');
+  }
+}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(body: CreateUserDto) {
+    const user = await this.userService.findOneByUsername(body.username);
+    if (!user)
+      throw new BadRequestException(
+        `No exist ${body.username} in our database`,
+      );
+
+    const isPasswordValid = bcrypt.compareSync(body.password, user.password);
+    if (!isPasswordValid) throw new BadRequestException('Invalid credentials');
+
+    const payload = {
+      token: this.jwtService.sign({ username: user.username }),
+    };
+    return payload;
+  }
+
+  async register(body: CreateUserDto) {
+    const user = await this.userService.findOneByUsername(body.username);
+    if (user) throw new BadRequestException('User already exists');
+
+    const userWithHashedPassword = {
+      ...body,
+      password: bcrypt.hashSync(body.password, bcrypt.genSaltSync(10)),
+    };
+
+    await this.userService.create(userWithHashedPassword);
+    const payload = {
+      token: this.jwtService.sign({ username: user.username }),
+    };
+    return payload;
+  }
+}
