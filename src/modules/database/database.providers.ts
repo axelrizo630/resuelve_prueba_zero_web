be4383186b56a4e4a7f9c1b3a2e68822843af40d
axelrizo <axel.rizo@seistreinta.com>
2023-11-04 16:21:15 -0500

    feat(database): add connection to database and modules

---
+++ src/modules/database/database.providers.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        entities: [User],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
